a684f19adf341f8fb1e80fc6336387f0e434c7fd
DmitriyLoginov666 <90390684+DmitriyLoginov666@users.noreply.github.com>
2024-05-20 10:05:15 +0300

    Add files via upload

---
+++ components/modules/Footer/FooterLinks.tsx
@@ -0,0 +1,16 @@
+const FooterLinks = () => (
+  <div className='footer__links'>
+    <span>
+      <a href='https://code.visualstudio.com/'>
+        https://code.visualstudio.com/
+      </a>
+    </span>
+    <span>
+      <a href='https://code.visualstudio.com/'>
+        https://code.visualstudio.com/
+      </a>
+    </span>
+  </div>
+)
+
+export default FooterLinks
